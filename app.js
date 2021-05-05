ba38f2ea560712cd1e4453bde455ded3097a394d
akash plackal <83161441+akash-plackal@users.noreply.github.com>
2021-05-05 12:26:43 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
-const lightBox = document.createElement('div');
-lightBox.id = 'lightBox';
-document.body.append(lightBox)
+const lightBox = document.createElement("div");
+lightBox.id = "lightBox";
+document.body.append(lightBox);
 
-const images = document.querySelectorAll('img');
-images.forEach(image => {
-    image.addEventListener('click', e => {
-        lightBox.classList.add('active');
-        const img = document.createElement('img');
-        img.src = image.src;
-        while(lightBox.firstChild) {
-            lightBox.removeChild(lightBox.firstChild)
-        }
-        lightBox.append(img)
-    })
-})
+const images = document.querySelectorAll("img");
+images.forEach((image) => {
+  image.addEventListener("click", (e) => {
+    lightBox.classList.add("active");
+    const img = document.createElement("img");
+    img.src = image.src;
+    while (lightBox.firstChild) {
+      lightBox.removeChild(lightBox.firstChild);
+    }
+    lightBox.append(img);
+  });
+});
 
-lightBox.addEventListener('click', e => {
-    lightBox.classList.remove('active')
-})
+lightBox.addEventListener("click", (e) => {
+  lightBox.classList.remove("active");
+});
